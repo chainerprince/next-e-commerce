306cd0ba7fad4f8eecadf3a20b45b704d9b6d403
Mutegetsi prince <mutegetsi2000@gmail.com>
2022-01-03 19:30:42 +0200

    work2

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -31,7 +31,7 @@ export default function Category({ data, query }) {
     <Layout>
       <div className={styles.container}>
         <Head>
-          <title>Create Next App</title>
+          <title>Murugo</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
